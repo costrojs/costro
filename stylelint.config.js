381c7cfa903303d80490add1092c4d2c48676f6c
Yoriiis <joris.daniel@gmail.com>
2021-11-11 20:42:45 +0100

    Fix stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
 	rules: {
 		indentation: ['tab', { ignore: 'inside-parens' }],
-		'unit-whitelist': ['em', 'rem', '%', 'px', 's', 'deg', 'fr', 'vh', 'vw', 'ms'],
+		'unit-allowed-list': ['em', 'rem', '%', 'px', 's', 'deg', 'fr', 'vh', 'vw', 'ms'],
 		'declaration-colon-newline-after': null,
 		'value-list-comma-newline-after': null,
 		'prettier/prettier': true
