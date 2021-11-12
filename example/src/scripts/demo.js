d0bd5d8cbd6f3de1b1fd1f3a450efea29ac54e51
Yoriiis <joris.daniel@gmail.com>
2021-11-12 21:09:39 +0100

    Remove old demo

--- example/src/scripts/demo.js
+++ example/src/scripts/demo.js
@@ -1,24 +1,70 @@
-import { Tunnel } from '../../../dist/tunnel';
-import Home from './home';
-import About from './about';
-import Contact from './contact';
+import { Tunnel, Link, Component, navigate } from '../../../dist/tunnel'
+import { createElement, Fragment } from 'jsx-dom'
+
+function Navigation() {
+	return (
+		<ul>
+			<li>
+				<Link to="/">Home</Link>
+				{/* <button onClick={() => navigate('/')}>Home</button> */}
+			</li>
+			<li>
+				<Link to="/about">About</Link>
+			</li>
+			<li>
+				<Link to="/contact">Contact</Link>
+			</li>
+		</ul>
+	)
+}
+
+function Home() {
+	return (
+		<div>
+			<Navigation />
+			<h2>Home</h2>
+		</div>
+	)
+}
+
+function About() {
+	return (
+		<>
+			<Navigation />
+			<h2>About</h2>
+		</>
+	)
+}
+
+class Contact extends Component {
+	render() {
+		return (
+			<>
+				<Navigation />
+				<h2>Contact</h2>
+			</>
+		)
+	}
+}
+
+const routes = [
+	{
+		path: '/',
+		component: Home
+	},
+	{
+		path: '/about',
+		component: About
+	},
+	{
+		path: '/contact',
+		component: Contact
+	}
+]
 
 // eslint-disable-next-line no-unused-vars
 const app = new Tunnel({
 	target: document.querySelector('#app'),
 	mode: 'history',
-	routes: [
-		{
-			path: '/',
-			component: Home
-		},
-		{
-			path: '/about',
-			component: About
-		},
-		{
-			path: '/contact',
-			component: Contact
-		}
-	]
-});
+	routes
+})
