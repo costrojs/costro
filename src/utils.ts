811d3933d501f96cc7cc9b5d3885fd43c80437c2
Yoriiis <joris.daniel@gmail.com>
2021-11-15 21:26:44 +0100

    Fix TS warnings on extend function

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,18 @@
-function extend(deep = false, ...objects) {
+function extend(deep: Boolean = false, ...objects: any[]): any {
 	const extended = {}
 
 	// Merge the object into the extended object
-	const merge = (obj) => {
+	const merge = (obj: any) => {
 		const keys = obj ? Object.keys(obj) : []
 		for (let i = 0, length = keys.length; i < length; i++) {
 			const key = keys[i]
 			if (Object.prototype.hasOwnProperty.call(obj, key)) {
 				// If deep merge and property is an object, merge properties
 				if (deep && Object.prototype.toString.call(obj[key]) === '[object Object]') {
+					// @ts-ignore
 					extended[key] = extend(true, extended[key], obj[key])
 				} else {
+					// @ts-ignore
 					extended[key] = obj[key]
 				}
 			}
