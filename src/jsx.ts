42bce0c8b74dfdb890f6602503531bd620c8d0d1
Yoriiis <joris.daniel@gmail.com>
2021-11-21 00:45:06 +0100

    Transform JSX to TS

--- src/jsx.ts
+++ src/jsx.ts
@@ -1,3 +1,11 @@
+import { Attributes, Component } from './interface'
+
+interface Constructable<T> {
+	new (...args: any): T
+}
+
+type fn = () => void
+
 const SVG_NAMESPACE = 'http://www.w3.org/2000/svg'
 const XML_NAMESPACE = 'http://www.w3.org/XML/1998/namespace'
 const SVG_ATTRIBUTES_CAMEL_CASE = ['xmlLang']
@@ -56,45 +64,69 @@ const SVG_TAGS = [
 	'tspan',
 	'use',
 	'view'
-]
+] // TODO: Can we remove this array?
 
-function createAttributes(element, name, value, isSvg = false) {
+function createAttributes(
+	element: HTMLElement | SVGElement,
+	name: string,
+	value: string | object | Node | boolean,
+	isSvg = false
+) {
+	const valusIsString = typeof value === 'string'
 	if (name === 'style') {
 		if (typeof value === 'string') {
-			element.style = value
+			element.setAttribute('style', value)
 		} else if (value instanceof Object) {
 			const styleProperty = Object.keys(value)
 			for (let i = 0, length = styleProperty.length; i < length; i++) {
+				// @ts-ignore
 				element.style[styleProperty[i]] = value[styleProperty[i]]
 			}
 		}
-	} else if (name === 'dataset') {
+	} else if (name === 'dataset' && value instanceof Object) {
 		const datasetProperty = Object.keys(value)
 		for (let i = 0, length = datasetProperty.length; i < length; i++) {
+			// @ts-ignore
 			element.dataset[datasetProperty[i]] = value[datasetProperty[i]]
 		}
-	} else if (name === 'innerHTML') {
+	} else if (name === 'innerHTML' && valusIsString) {
 		element.innerHTML = value
-	} else if (['class', 'className'].includes(name)) {
+	} else if (['class', 'className'].includes(name) && valusIsString) {
 		const cssClass = value.split(' ')
 		for (let i = 0, length = cssClass.length; i < length; i++) {
 			element.classList.add(cssClass[i])
 		}
-	} else if (isSvg && SVG_ATTRIBUTES_CAMEL_CASE.includes(name)) {
+	} else if (isSvg && SVG_ATTRIBUTES_CAMEL_CASE.includes(name) && valusIsString) {
 		const attributesWithColon = name.replaceAll(/[A-Z]/g, (match) => ':' + match.toLowerCase())
 		element.setAttributeNS(XML_NAMESPACE, attributesWithColon, value)
-	} else if (value !== false && value !== null) {
+	} else if (value === true) {
+		element.setAttribute(name, '')
+	} else if (value !== false && value !== null && valusIsString) {
 		element.setAttribute(name, value)
 	}
 }
 
-function Fragment(tag) {
+interface FragmentTag {
+	children: HTMLElement[] | SVGElement[]
+}
+
+interface ElementAttributes {
+	[key: string]: string | (() => void)
+}
+
+function Fragment(tag: FragmentTag) {
+	// console.log('Fragment', tag)
 	const fragment = document.createDocumentFragment()
 	tag.children && tag.children.length && appendChildren(fragment, tag.children)
 	return fragment
 }
 
-function createElement(tag, attributes = {}, ...children) {
+function createElement(
+	tag: string | Function,
+	attributes: null | ElementAttributes,
+	...children: string[] | HTMLElement[]
+) {
+	// console.log('createElement', { tag, attributes, children })
 	let element
 	if (tag instanceof Function) {
 		element = tag(attributes || {})
@@ -108,6 +140,7 @@ function createElement(tag, attributes = {}, ...children) {
 				const name = attributeName[i]
 				const value = attributes[name]
 				if (name.startsWith('on') && value instanceof Function) {
+					// @ts-ignore
 					element[name.toLowerCase()] = value
 				} else {
 					createAttributes(element, name, value, isSvg)
@@ -116,12 +149,11 @@ function createElement(tag, attributes = {}, ...children) {
 		}
 	}
 
-	// TODO: empty node ?
 	children.length && appendChildren(element, children)
 
 	if (tag instanceof Function) {
-		if (isComponentClass(tag)) {
-			element = initComponentClass(tag, attributes, children)
+		if (isComponentClass(tag as fn)) {
+			element = initComponentClass(tag as Constructable<Component>, attributes, children)
 		} else {
 			element = tag({ ...attributes, children })
 		}
@@ -130,7 +162,10 @@ function createElement(tag, attributes = {}, ...children) {
 	return element
 }
 
-function appendChildren(element, children) {
+function appendChildren(
+	element: DocumentFragment | HTMLElement | SVGElement,
+	children: Array<string | Node>
+) {
 	for (let i = 0, length = children.length; i < length; i++) {
 		const child = children[i]
 		if (typeof child === 'string') {
@@ -143,15 +178,21 @@ function appendChildren(element, children) {
 	return element
 }
 
-function isComponentClass(tag) {
-	const { prototype } = tag
+function isComponentClass(tag: fn): boolean {
+	const prototype = tag.prototype
+	console.log(
+		'isComponentClass',
+		tag,
+		!!(prototype && (prototype.isReactComponent || prototype.__isComponent))
+	)
 	return !!(prototype && (prototype.isReactComponent || prototype.__isComponent))
 }
 
-function initComponentClass(Tag, attributes, children) {
+function initComponentClass(Tag: Constructable<Component>, attributes: any, children: Array<any>) {
+	console.log('initComponentClass', { Tag, attributes, children })
 	attributes = { ...attributes, children }
 	const instance = new Tag(attributes)
 	return instance.render()
 }
 
-export { createElement, Fragment }
+export { createElement, createElement as h, Fragment, Fragment as F }
