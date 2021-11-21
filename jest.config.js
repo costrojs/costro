5d77094569143d5d402f6a559a0300a586a6b7fd
Yoriiis <joris.daniel@gmail.com>
2021-11-21 19:54:32 +0100

    Update Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-	moduleFileExtensions: ['js', 'css'],
+	coverageDirectory: 'coverage',
+	moduleFileExtensions: ['js', 'ts', 'css'],
+	modulePaths: ['./src'],
+	preset: 'ts-jest/presets/js-with-babel',
 	resetModules: true,
+	testEnvironment: 'jsdom',
+	transform: {
+		'^.+\\.(ts|tsx|js)$': 'ts-jest'
+	},
+	transformIgnorePatterns: ['node_modules/(?!jsx-dom)'],
 	verbose: true
 }
