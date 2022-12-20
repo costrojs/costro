6ba9ddd3df3f02eee76017f251f0e92fe0f1957b
Yoriiis <joris.daniel@gmail.com>
2022-12-20 18:24:43 +0100

    Rewrite jsx unit tests

--- tests/unit/jsx.test.js
+++ tests/unit/jsx.test.js
@@ -83,280 +83,130 @@ afterEach(() => {
 	document.body.innerHTML = ''
 })
 
-describe('createElement', () => {
-	it('Should call the createElement function', () => {
-		const result = createElement(
-			'div',
-			{
-				class: 'container',
-				'data-track': true,
-				dataset: {
-					name: 'element',
-					params: 1
-				},
-				innerHTML: '<span class="span"><em class="em">Hello</em></span>',
-				style: 'align-items: center; display: flex;'
-			},
-			getDivChildren()
-		)
+describe('JSX', () => {
+	describe('createElement', () => {
+		it('Should call the createElement function with a string tag and without attribute', () => {
+			const result = createElement('div', null)
 
-		expect(result).toStrictEqual(element)
-		expect(result.isEqualNode(element)).toBe(true)
-		expect(result.outerHTML).toStrictEqual(element.outerHTML)
-	})
-
-	it('Should call the createElement function with style object', () => {
-		const result = createElement(
-			'div',
-			{
-				class: 'container',
-				'data-track': true,
-				dataset: {
-					name: 'element',
-					params: 1
-				},
-				innerHTML: '<span class="span"><em class="em">Hello</em></span>',
-				style: {
-					alignItems: 'center',
-					display: 'flex'
-				}
-			},
-			getDivChildren()
-		)
-
-		expect(result).toStrictEqual(element)
-		expect(result.isEqualNode(element)).toBe(true)
-		expect(element.outerHTML).toEqual(result.outerHTML)
-	})
-
-	it('Should call the createElement function with event listener', () => {
-		const onClick = () => {
-			/* Empty */
-		}
-		const onSubmit = () => {
-			/* Empty */
-		}
-
-		const result = createElement(
-			'div',
-			{
-				class: 'container',
-				'data-track': true,
-				dataset: {
-					name: 'element',
-					params: 1
-				},
-				innerHTML: '<span class="span"><em class="em">Hello</em></span>',
-				onClick,
-				onSubmit,
-				style: 'align-items: center; display: flex;'
-			},
-			getDivChildren()
-		)
-
-		expect(result.onclick).toBeInstanceOf(Function)
-		expect(result.onclick).toStrictEqual(onClick)
-		expect(result.onsubmit).toBeInstanceOf(Function)
-		expect(result.onsubmit).toStrictEqual(onSubmit)
-	})
-
-	it('Should call the createElement function with a tag function', () => {
-		const tagFn = () => {
-			return getElement()
-		}
-
-		const result = createElement(tagFn, {
-			class: 'container',
-			'data-track': true,
-			dataset: {
-				name: 'element',
-				params: 1
-			},
-			innerHTML: '<span class="span"><em class="em">Hello</em></span>',
-			style: 'align-items: center; display: flex;'
+			expect(result.outerHTML).toStrictEqual('<div></div>')
 		})
 
-		expect(result).toStrictEqual(element)
-		expect(result.isEqualNode(element)).toBe(true)
-		expect(element.outerHTML).toStrictEqual(result.outerHTML)
-	})
-
-	it('Should call the createElement function with a tag function and no attributes', () => {
-		const div = document.createElement('div')
-		const tagFn = () => {
-			return div
-		}
-
-		const result = createElement(tagFn, false)
-
-		expect(result).toStrictEqual(div)
-		expect(result.isEqualNode(div)).toBe(true)
-		expect(div.outerHTML).toStrictEqual(result.outerHTML)
-	})
-
-	it('Should call the createElement function with a class and __isComponent prototype and attributes', () => {
-		class CustomComponent {
-			render() {
-				return getElement()
-			}
-		}
-		CustomComponent.prototype.__isComponent = true
-
-		const result = createElement(CustomComponent, {
-			class: 'container',
-			'data-track': true,
-			dataset: {
-				name: 'element',
-				params: 1
-			},
-			innerHTML: '<span class="span"><em class="em">Hello</em></span>',
-			style: 'align-items: center; display: flex;'
-		})
-
-		expect(result).toStrictEqual(element)
-		expect(result.isEqualNode(element)).toBe(true)
-		expect(element.outerHTML).toStrictEqual(result.outerHTML)
-	})
-
-	it('Should call the createElement function with a class and isReactComponent prototype and attributes', () => {
-		class CustomComponent {
-			render() {
-				return getElement()
-			}
-		}
-		CustomComponent.prototype.isReactComponent = {}
-
-		const result = createElement(CustomComponent, {
-			class: 'container',
-			'data-track': true,
-			dataset: {
-				name: 'element',
-				params: 1
-			},
-			innerHTML: '<span class="span"><em class="em">Hello</em></span>',
-			style: 'align-items: center; display: flex;'
-		})
-
-		expect(result).toStrictEqual(element)
-		expect(result.isEqualNode(element)).toBe(true)
-		expect(element.outerHTML).toStrictEqual(result.outerHTML)
-	})
-
-	it('Should call the createElement function with a class and __isComponent prototype and a nested component', () => {
-		class NestedCustomComponent {
-			constructor(props) {
-				this.props = props
-			}
-
-			render() {
-				return <h2>{this.props.title}</h2>
-			}
-		}
-		NestedCustomComponent.prototype.__isComponent = true
-
-		class CustomComponent {
-			render() {
-				return <NestedCustomComponent title="Nested component" />
-			}
-		}
-		CustomComponent.prototype.__isComponent = true
-
-		const result = createElement(CustomComponent)
-
-		expect(result).toStrictEqual(<h2>Nested component</h2>)
-		expect(result.isEqualNode(<h2>Nested component</h2>)).toBe(true)
-		expect((<h2>Nested component</h2>).outerHTML).toStrictEqual(result.outerHTML)
-	})
-
-	it('Should call the createElement function with a class and __isComponent prototype and no attributes', () => {
-		class CustomComponent {
-			render() {
-				return getElement()
-			}
-		}
-		CustomComponent.prototype.__isComponent = true
-
-		const result = createElement(CustomComponent)
-
-		expect(result).toStrictEqual(element)
-		expect(result.isEqualNode(element)).toBe(true)
-		expect(element.outerHTML).toStrictEqual(result.outerHTML)
-	})
-
-	it('Should call the createElement function with a child as a string', () => {
-		const div = document.createElement('div')
-		div.appendChild(document.createTextNode('Hello'))
-
-		const result = createElement('div', {}, 'Hello')
-
-		expect(result).toStrictEqual(div)
-		expect(result.isEqualNode(div)).toBe(true)
-		expect(div.outerHTML).toStrictEqual(result.outerHTML)
-	})
-
-	it('Should call the createElement function with a child as a number', () => {
-		const div = document.createElement('div')
-		div.appendChild(document.createTextNode(1))
-
-		const result = createElement('div', {}, 1)
-
-		expect(result).toStrictEqual(div)
-		expect(result.isEqualNode(div)).toBe(true)
-		expect(div.outerHTML).toStrictEqual(result.outerHTML)
-	})
-
-	it('Should call the createElement function with SVG', () => {
-		const result = createElement(
-			'svg',
-			{
-				height: '50px',
-				version: '1.1',
+		it('Should call the createElement function with a string svg tag and without attribute', () => {
+			const result = createElement('svg', {
 				viewBox: '0 0 48 48',
-				width: '50px',
-				x: '0',
-				xmlns: SVG_NAMESPACE,
-				y: '0'
-			},
-			getSvgChildren()
-		)
+				xmlLang: 'fr'
+			})
 
-		expect(result).toStrictEqual(svg)
-		expect(result.isEqualNode(svg)).toBe(true)
-		expect(svg.outerHTML).toStrictEqual(result.outerHTML)
-	})
-
-	it('Should call the createElement function with SVG and special camel case attributes', () => {
-		const svgElement = document.createElementNS(SVG_NAMESPACE, 'svg')
-		svgElement.setAttributeNS('http://www.w3.org/XML/1998/namespace', 'xml:lang', 'en-US')
-
-		const result = createElement('svg', {
-			xmlLang: 'en-US'
+			expect(result.outerHTML).toStrictEqual('<svg viewBox="0 0 48 48" xml:lang="fr"></svg>')
 		})
 
-		expect(result).toStrictEqual(svgElement)
-		expect(result.isEqualNode(svgElement)).toBe(true)
-		expect(result.outerHTML).toStrictEqual(svgElement.outerHTML)
-	})
-})
+		it('Should call the createElement function with a string tag and attributes', () => {
+			const onClickFn = () => {}
+			const result = createElement('div', {
+				className: 'box',
+				dataset: { foo: 'bar' },
+				'data-track': true,
+				'data-track-params': 'home',
+				'data-track-id': 1,
+				'data-ignore-boolean': false,
+				'data-ignore-null': null,
+				style: 'display: flex; align-items: center;',
+				innerHTML: '<span>Inner</span>',
+				onClick: onClickFn
+			})
 
-describe('Fragment', () => {
-	it('Should call the Fragment function', () => {
-		const result = Fragment()
-
-		expect(result).toStrictEqual(document.createDocumentFragment())
-	})
-
-	it('Should call the Fragment function with children', () => {
-		const div = document.createElement('div')
-
-		const result = Fragment({
-			children: [div, 'Hello']
+			expect(result.outerHTML).toStrictEqual(
+				'<div class="box" data-foo="bar" data-track="" data-track-params="home" data-track-id="1" style="display: flex; align-items: center;"><span>Inner</span></div>'
+			)
+			expect(result.onclick).toStrictEqual(onClickFn)
+			expect(result.dataset.foo).toStrictEqual('bar')
 		})
 
-		const fragment = document.createDocumentFragment()
-		fragment.appendChild(document.createElement('div'))
-		fragment.appendChild(document.createTextNode('Hello'))
+		it('Should call the createElement function with a string tag and style attribute in object format', () => {
+			const result = createElement('div', {
+				style: {
+					display: 'flex',
+					alignItems: 'center'
+				}
+			})
 
-		expect(result).toStrictEqual(fragment)
+			expect(result.outerHTML).toStrictEqual(
+				'<div style="display: flex; align-items: center;"></div>'
+			)
+		})
+
+		it('Should call the createElement function with a string tag and children as node', () => {
+			const list = [0, 1, 3].map((item) => <li>{item}</li>)
+			const button = document.createElement('button')
+			button.insertAdjacentHTML('beforeend', '<span>Inner</span>')
+			button.classList.add('button')
+			const result = createElement('div', null, list, button)
+
+			expect(result.outerHTML).toStrictEqual(
+				'<div><li>0</li><li>1</li><li>3</li><button class="button"><span>Inner</span></button></div>'
+			)
+		})
+
+		it('Should call the createElement function with a string tag and children as string', () => {
+			const button = document.createElement('button')
+			button.insertAdjacentHTML('beforeend', 'Click me')
+			const result = createElement('div', null, button, 'Text')
+
+			expect(result.outerHTML).toStrictEqual('<div><button>Click me</button>Text</div>')
+		})
+
+		it('Should call the createElement function with a component class with __isComponent marker', () => {
+			class CustomComponent {
+				render() {
+					return document.createElement('div')
+				}
+			}
+			CustomComponent.prototype.__isComponent = true
+
+			const result = createElement(CustomComponent)
+
+			expect(result.outerHTML).toStrictEqual('<div></div>')
+		})
+
+		it('Should call the createElement function with a function as component class with isReactComponent marker', () => {
+			class CustomComponent {
+				render() {
+					return document.createElement('div')
+				}
+			}
+			CustomComponent.prototype.isReactComponent = true
+
+			const result = createElement(CustomComponent)
+
+			expect(result.outerHTML).toStrictEqual('<div></div>')
+		})
+
+		it('Should call the createElement function with a function', () => {
+			const result = createElement(() => '<button>Click me</button>')
+
+			expect(result).toStrictEqual('<button>Click me</button>')
+		})
+	})
+
+	describe('Fragment', () => {
+		it('Should call the Fragment function', () => {
+			const result = Fragment()
+
+			expect(result).toStrictEqual(document.createDocumentFragment())
+		})
+
+		it('Should call the Fragment function with children', () => {
+			const div = document.createElement('div')
+
+			const result = Fragment({
+				children: [div, 'Hello']
+			})
+
+			const fragment = document.createDocumentFragment()
+			fragment.appendChild(document.createElement('div'))
+			fragment.appendChild(document.createTextNode('Hello'))
+
+			expect(result).toStrictEqual(fragment)
+		})
 	})
 })
